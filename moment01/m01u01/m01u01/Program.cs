bde9f21d915be1327ec863eb5885ee2ad2fe6b8d
Isak Wellin <68118710+isakwellin@users.noreply.github.com>
2026-01-14 21:27:27 +0100

    Add files via upload

---
+++ moment01/m01u01/m01u01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Isak Wellin, 23TEa, m01u01, 2026-01-14
+
+namespace m01u01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jag heter Isak Wellin och jag läser programmering på Alströmergymnasiet.");
+
+            //Dubbelfnuttar skrivs ut genom att sätta ett omvänt snedstreck framför dubbelfnutten
+            Console.WriteLine("Jag tycker att det verkar \"kul\" med programmering.");
+
+            //Raderna skrivs ut med en enda instruktion genom att använda sig av \n radbrytningen
+            Console.WriteLine("Dessa tre rader\nhar jag skrivit med\nen enda instruktion.");
+
+            //Tabb användes innan stjärntecknena för att göra listan
+            Console.WriteLine("Följande saker har jag kommenterat i min kod:\n  * Hur jag skrev ut dubbelfnuttarna på andra raden\n  * Hur jag kunde skriva ut tre rader med en enda instruktion.\n  * Hur jag använde mig av tab, för att göra denna listan.");
+        }
+    }
+}
