faca6eb9b41075a1d4c60ec6e874c69fdb525c0e
Isak Wellin <68118710+isakwellin@users.noreply.github.com>
2026-01-17 22:37:59 +0100

    m01u04

---
+++ moment01/m01u04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Isak Wellin, 23TEa, m01u04, 2026-01-17
+
+namespace m01u04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            int hour = dt.Hour;
+            Console.WriteLine(hour);
+            
+            if(hour > 8 & hour < 16)
+            {
+                Console.WriteLine("Skoldagen pågår");
+            }
+            else if (hour > 0 & hour < 7)
+            {
+                Console.WriteLine("Skoldagen har inte börjat");
+            }
+            else
+            {
+                Console.WriteLine("Skoldagen är slut");
+            }
+        }
+    }
+}
